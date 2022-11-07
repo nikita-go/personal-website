fc0db461927a665d10a18cc80a267a04db5a6bb6
Nikita Gorshkov <gorshkovnikita@gmail.com>
2022-11-07 21:55:50 +0800

    Add 'projects' page

--- pages/components/global/Footer.jsx
+++ pages/components/global/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Footer = () => {
     return(
-        <nav className="bg-[#101010] w-full flex justify-between items-center bottom">
+        <nav className="bg-[#101010] w-screen flex justify-between items-center bottom">
             <div className="mx-auto flex my-6 w-full max-w-[90%] justify-between">
                 <p className="text-white text-lg font-semibold">© Nikita Gorshkov 2022</p>
                 <ul className="list-none text-[#648ccd] text-lg space-x-12">
