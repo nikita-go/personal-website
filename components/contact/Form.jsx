1839ed4ff376107ab5653f972d028eaa83fffd8e
Nikita Gorshkov <gorshkovnikita@gmail.com>
2022-11-08 20:06:52 +0800

    Make contact form work

--- components/contact/Form.jsx
+++ components/contact/Form.jsx
@@ -4,13 +4,13 @@ export const Form = () => {
     return (
         <div>
             <h1 className="text-white font-bold text-6xl">Contact Me</h1>
-            <form className="my-4">
+            <form action="https://getform.io/f/77f23fa5-cef6-4f70-b2af-d7646794b7f8" method="post" className="my-4">
                 <div className="grid grid-cols-2 gap-4 w-[75vh]">
-                    <input type="text" placeholder="Name (required)" className="bg-zinc-900 rounded-md border-transparent focus:ring-0 focus:border-zinc-600" required />
-                    <input type="text" placeholder="Email" className="bg-zinc-900 rounded-md border-transparent focus:ring-0 focus:border-zinc-600" />
-                    <textarea placeholder="Message (required)" className="bg-zinc-900 col-start-1 col-end-3 resize-none h-[30vh] rounded-md border-transparent focus:ring-0 focus:border-zinc-600" required />
+                    <input name="name" type="text" placeholder="Name (required)" className="bg-zinc-900 rounded-md border-transparent focus:ring-0 focus:border-zinc-600" required />
+                    <input name="email" type="email"  placeholder="Email" className="bg-zinc-900 rounded-md border-transparent focus:ring-0 focus:border-zinc-600" />
+                    <textarea name="message" placeholder="Message (required)" className="bg-zinc-900 col-start-1 col-end-3 resize-none h-[30vh] rounded-md border-transparent focus:ring-0 focus:border-zinc-600" required />
                 </div>
-                <button type="submit" className="px-3 py-2 bg-zinc-900 hover:bg-zinc-800 rounded-lg my-4">Submit</button>
+                <button type="submit" className="px-3 py-2 bg-zinc-900 hover:bg-zinc-800 rounded-lg">Submit</button>
             </form>
         </div>
     )
