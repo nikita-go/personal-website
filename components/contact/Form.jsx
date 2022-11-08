6acfd402ea610c555a6a7162904fcdbbb949dbf2
Nikita Gorshkov <gorshkovnikita@gmail.com>
2022-11-08 18:55:55 +0800

    Make form nicer

--- components/contact/Form.jsx
+++ components/contact/Form.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export const Form = () => {
     return (
         <div>
-            <h1 className="text-white font-bold text-6xl my-4">Contact Me</h1>
-            <form>
+            <h1 className="text-white font-bold text-6xl">Contact Me</h1>
+            <form className="my-4">
                 <div className="grid grid-cols-2 gap-4 w-[75vh]">
-                    <input type="text" id="name" placeholder="Name" className="bg-zinc-900 rounded-md border-transparent" />
-                    <input type="text" id="email" placeholder="Email" className="bg-zinc-900 rounded-md border-transparent" />
-                    <textarea id="message" placeholder="Message" className="bg-zinc-900 col-start-1 col-end-3 resize-none h-[30vh] rounded-md border-transparent" />
+                    <input type="text" placeholder="Name (required)" className="bg-zinc-900 rounded-md border-transparent focus:ring-0 focus:border-zinc-600" required />
+                    <input type="text" placeholder="Email" className="bg-zinc-900 rounded-md border-transparent focus:ring-0 focus:border-zinc-600" />
+                    <textarea placeholder="Message (required)" className="bg-zinc-900 col-start-1 col-end-3 resize-none h-[30vh] rounded-md border-transparent focus:ring-0 focus:border-zinc-600" required />
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit" className="px-3 py-2 bg-zinc-900 hover:bg-zinc-800 rounded-lg my-4">Submit</button>
             </form>
         </div>
     )
