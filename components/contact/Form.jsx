04402b0f00c5028f2eb4309e8fdd23d661a35eb4
Nikita Gorshkov <gorshkovnikita@gmail.com>
2022-12-29 13:15:40 +0800

    Fix input text color

--- components/contact/Form.jsx
+++ components/contact/Form.jsx
@@ -4,7 +4,7 @@ export const Form = () => {
     return (
         <div>
             <h1 className="text-white font-bold text-6xl">Contact Me</h1>
-            <form action="https://getform.io/f/77f23fa5-cef6-4f70-b2af-d7646794b7f8" method="post" className="my-4">
+            <form action="https://getform.io/f/77f23fa5-cef6-4f70-b2af-d7646794b7f8" method="post" className="my-4 text-white">
                 <div className="grid grid-cols-2 gap-4 w-[75vh]">
                     <input name="name" type="text" placeholder="Name (required)" className="bg-zinc-900 rounded-md border-transparent focus:ring-0 focus:border-zinc-600" required />
                     <input name="email" type="email"  placeholder="Email" className="bg-zinc-900 rounded-md border-transparent focus:ring-0 focus:border-zinc-600" />
