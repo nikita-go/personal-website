176dca47e0ff9b53282f44f6eb579963ce648ae5
Nikita Gorshkov <gorshkovnikita@gmail.com>
2022-12-30 21:30:36 +0800

    Change transition duration

--- components/global/Navbar.jsx
+++ components/global/Navbar.jsx
@@ -5,8 +5,8 @@ export const Navbar = () => {
     return(
         <div className="flex items-center">
             <ul className="list-none text-[#ff7357] text-lg space-x-12">
-                <Link href="/posts" className="hover:text-[#ff866d] ease-in-out transition duration-250">posts</Link>
-                <Link href="/projects" className="hover:text-[#ff866d] ease-in-out transition duration-250">projects</Link>
+                <Link href="/posts" className="hover:text-[#ff866d] ease-in-out transition duration-300">posts</Link>
+                <Link href="/projects" className="hover:text-[#ff866d] ease-in-out transition duration-300">projects</Link>
             </ul>
         </div>
     )
