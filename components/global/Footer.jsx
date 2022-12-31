53c02a9b9a922734a2cf4d8d0310e2e161baa44a
Nikita Gorshkov <gorshkovnikita@gmail.com>
2022-12-31 16:28:56 +0800

    Fix positioning

--- components/global/Footer.jsx
+++ components/global/Footer.jsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 export const Footer = () => {
     return(
-        <nav className="bg-[#101010] absolute w-full flex justify-between items-center left-0 bottom-0">
-            <div className="mx-auto flex my-6 w-full max-w-[90%] justify-between">
-                <p className="text-white text-lg font-semibold">© Nikita Gorshkov 2022</p>
-                <ul className="list-none text-[#648ccd] text-lg space-x-12">
-                    <Link href="/contact" className="hover:text-[#7d9dd0] ease-in-out transition duration-300">Contact me</Link>
-                </ul>
-            </div>
-        </nav> 
+        <footer className="mt-auto">
+            <nav className="bg-[#101010] w-full flex justify-between items-center">
+                <div className="mx-auto flex my-6 w-full max-w-[90%] justify-between">
+                    <p className="text-white text-lg font-semibold">© Nikita Gorshkov 2022</p>
+                    <ul className="list-none text-[#648ccd] text-lg space-x-12">
+                        <Link href="/contact" className="hover:text-[#7d9dd0] ease-in-out transition duration-300">Contact me</Link>
+                    </ul>
+                </div>
+            </nav> 
+        </footer>
     )
 }
--- components/global/Header.jsx
+++ components/global/Header.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Link from "next/link";
 import { Navbar } from "./Navbar";
+import {useEffect, useState, useRef} from 'react';
 
 export function Header() {
+    const [height, setHeight] = useState(0);
+    const ref = useRef(null);
+    useEffect(() => {
+      setHeight(ref.current.offsetHeight);
+    }, []);
+
     return(
-        <nav className="fixed z-10 flex min-w-full justify-between items-center backdrop-blur-lg">
-            <div className="mx-auto my-[20px] flex w-full max-w-[90%] justify-between">
-                <Link href="/" className="text-white font-bold text-3xl">nikitago.xyz</Link>
-                <Navbar />
-            </div>
-        </nav>
+        <>
+            <nav ref={ref} className="fixed z-10 top-0 flex min-w-full justify-between items-center backdrop-blur-md">
+                <div className="mx-auto my-[20px] flex w-full max-w-[90%] justify-between">
+                    <Link href="/" className="text-white font-bold text-3xl">nikitago.xyz</Link>
+                    <Navbar />
+                </div>
+            </nav>
+            <div className={`h-[${height}px]`}></div>
+        </>
     )
 }
