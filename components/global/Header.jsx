5586908bdd1c2a8cfda046a81a0cb1df44db38cb
Nikita Gorshkov <gorshkovnikita@gmail.com>
2022-12-31 18:33:26 +0800

    Fix header

--- components/global/Header.jsx
+++ components/global/Header.jsx
@@ -5,7 +5,7 @@ import { Navbar } from "./Navbar";
 export function Header() {
     return(
         <>
-            <nav ref={ref} className="fixed z-10 top-0 flex min-w-full justify-between items-center backdrop-blur-md">
+            <nav className="fixed z-10 top-0 flex min-w-full justify-between items-center backdrop-blur-md">
                 <div className="mx-auto my-[20px] flex w-full max-w-[90%] justify-between">
                     <Link href="/" className="text-white font-bold text-3xl">nikitago.xyz</Link>
                     <Navbar />
