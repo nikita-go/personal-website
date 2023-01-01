7806bbe7e0698d5a704170b0c79340368f035292
Nikita Gorshkov <gorshkovnikita@gmail.com>
2023-01-01 18:34:16 +0800

    Fix error handling

--- components/contact/Form.jsx
+++ components/contact/Form.jsx
@@ -6,9 +6,10 @@ export const Form = () => {
     const [userName, setUserName] = useState('');
     const [userEmail, setUserEmail] = useState('');
     const [userMessage, setUserMessage] = useState('');
-    const [ok, setOk] = useState(true);
+    const [error, setError] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const submission = {
             name: userName,
@@ -16,7 +17,7 @@ export const Form = () => {
             message: userMessage,
         }
         try {
-            axios({
+            await axios({
                 method: "post",
                 url: "https://getform.io/f/77f23fa5-cef6-4f70-b2af-d7646794b7f8",
                 data: submission,
@@ -24,12 +25,15 @@ export const Form = () => {
             setUserName('');
             setUserEmail('');
             setUserMessage('');
-            setOk(true);
+            setError(false);
+            setSubmitted(true);
         }
-        catch {
-            setOk(false);
+        catch(err) {
+            setSubmitted(false);
+            setError(true);
         } 
     }
+
     return (
         <div>
             <h1 className="text-white font-bold text-6xl">Contact Me</h1>
@@ -58,10 +62,15 @@ export const Form = () => {
                         className="mt-2 bg-zinc-900 col-start-1 col-end-3 resize-none h-[30vh] rounded-md border-transparent focus:ring-0 focus:border-zinc-600" 
                         required />
                 </div>
-                <button type="submit" className="px-3 py-2 my-4 bg-zinc-900 hover:bg-zinc-800 rounded-lg">Submit</button>
-                {!ok && 
-                    <p className="text-white">Unable to send your submission.</p>
-                }
+                <div className="flex flex-row items-center gap-x-4">
+                    <button type="submit" className="px-3 py-2 my-4 bg-zinc-900 hover:bg-zinc-800 rounded-lg">Submit</button>
+                    {error && 
+                        <p className="text-[#ff6f6f]">Unable to send your submission. Please try again later.</p>
+                    }
+                    {submitted && 
+                        <p className="text-[#6fff8c]">Your submission has been successfully sent.</p>
+                    }
+                </div>
             </form>
         </div>
     )
