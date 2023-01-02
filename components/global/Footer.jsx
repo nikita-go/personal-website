01c13dcb6b95d4a60ec8cfa89202df8d1ae03db2
Nikita Gorshkov <gorshkovnikita@gmail.com>
2023-01-02 19:10:48 +0800

    Update year

--- components/global/Footer.jsx
+++ components/global/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
         <footer className="mt-auto">
             <nav className="bg-[#101010] w-full flex justify-between items-center">
                 <div className="mx-auto flex my-6 w-full max-w-[90%] justify-between">
-                    <p className="text-white text-lg font-semibold">© Nikita Gorshkov 2022</p>
+                    <p className="text-white text-lg font-semibold">© Nikita Gorshkov 2023</p>
                     <ul className="list-none text-[#648ccd] text-lg space-x-12">
                         <Link href="/contact" className="hover:text-[#7d9dd0] ease-in-out transition duration-300">Contact me</Link>
                     </ul>
