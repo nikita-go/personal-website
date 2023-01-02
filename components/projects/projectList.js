d31d387716aca50e24053125c9e07a43b03c6053
Nikita Gorshkov <gorshkovnikita@gmail.com>
2023-01-02 19:06:49 +0800

    Add weather app project

--- components/projects/projectList.js
+++ components/projects/projectList.js
@@ -3,9 +3,9 @@ const projectList = [
         name: "Mock Weather App",
         link: "https://weather-app-nikita-go.vercel.app/",
         github: "nikita-go/weather-app",
-        description: "A simple mock weather app. Built with React, Next.js, TailwindCSS, and Axios.",
-        startColor: "a8c0ff",
-        endColor: "3f2b96",
+        description: "A simple mock weather app that displays all the information you need. Built with React, Next.js, TailwindCSS, and Axios.",
+        startColor: "from-[#556270]",
+        endColor: "to-[#ff6b6b]",
     },
 ];
 
