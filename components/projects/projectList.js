014dbb9380597e846181937c873cfde7c8b45cb0
Nikita Gorshkov <gorshkovnikita@gmail.com>
2023-01-02 16:53:46 +0800

    Create project list

---
+++ components/projects/projectList.js
@@ -0,0 +1,12 @@
+const projectList = [
+    {
+        name: "Mock Weather App",
+        link: "https://weather-app-nikita-go.vercel.app/",
+        github: "nikita-go/weather-app",
+        description: "A simple mock weather app. Built with React, Next.js, TailwindCSS, and Axios.",
+        startColor: "FC5C7D",
+        endColor: "6A82FB",
+    },
+];
+
+export default projectList;
