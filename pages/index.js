d5a74d9b26cdcab8f0f7022386dfc4744208d7e8
Nikita Gorshkov <gorshkovnikita@gmail.com>
2023-01-18 15:02:51 +0800

    change intro text

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
               <br />
               <p> I'm currently a web developer for the <a href="https://joincpi.org/" className="font-semibold text-purple-600">Competitive Programming Initiative</a> and <a href="https://www.cerealcodes.org/" className="font-semibold"><span className="text-sky-500">Cereal</span><span className="text-green-600">Codes</span></a>.</p>
               <br />
-              <p>I'll be adding more to this website very soon since I currently have some free time.</p>
+              <p>I'll be adding more to this website soon, so please keep an eye out for them!</p>
               <br />
               <p>In the meantime, check out my GitHub profile (<a href="https://github.com/nikita-go" className="font-semibold text-transparent text-animation">@nikita-go</a>)!</p>
             </div>
