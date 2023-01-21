a711e309ba013f1ef21769874e1837ecbe853a30
Nikita Gorshkov <gorshkovnikita@gmail.com>
2023-01-21 20:39:39 +0800

    delete file

--- pages/tags/[tag].js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Head from "next/head";
-import fs from "fs";
-import matter from "gray-matter";
-import { Header } from "../../components/global/Header";
-import { Footer } from "../../components/global/Footer";
-import Link from "next/link";
