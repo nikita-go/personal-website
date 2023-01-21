d781296f76bbb82bec26b5ab337e3eb04fd341ff
Nikita Gorshkov <gorshkovnikita@gmail.com>
2023-01-21 11:15:04 +0800

    create tags page

---
+++ pages/tags/[tag].js
@@ -0,0 +1,7 @@
+import React from "react";
+import Head from "next/head";
+import fs from "fs";
+import matter from "gray-matter";
+import { Header } from "../../components/global/Header";
+import { Footer } from "../../components/global/Footer";
+import Link from "next/link";
