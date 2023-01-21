5d3fa7c14f2e3ce3f67cb94807d33330421db27d
Nikita Gorshkov <gorshkovnikita@gmail.com>
2023-01-21 11:14:42 +0800

    change tag link

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -28,7 +28,7 @@ export default function Posts(props) {
                                         <div>
                                             {post.data.tags.map((tag, tagIndex) => (
                                                 <span key={tag} className="text-[#5187df] hover:text-[#5d9bff] ease-in-out transition duration-300">
-                                                    <Link href={`/posts?tag=${tag}`}>{tag}</Link>
+                                                    <Link href={`/tags/${tag}`}>{tag}</Link>
                                                     {tagIndex!=post.data.tags.length-1 && <span className="mx-1">•</span>}
                                                 </span>
                                             ))}
